cd0d7b18077f2e95e091dfdd6146b540fd22eb73
dccmx <dccmx@dccmx.com>
2013-07-15 11:36:35 +0800

    usr rsyncd.conf

--- manifests/server.pp
+++ manifests/server.pp
@@ -21,7 +21,7 @@ class rsync::server(
       bind        => $address,
       port        => '873',
       server      => '/usr/bin/rsync',
-      server_args => '--daemon --config /etc/rsync.conf',
+      server_args => '--daemon --config /etc/rsyncd.conf',
       require     => Package['rsync'],
     }
   } else {
@@ -52,7 +52,7 @@ class rsync::server(
   # which happens with cobbler systems by default
   exec { 'compile fragments':
     refreshonly => true,
-    command     => "ls ${rsync_fragments}/frag-* 1>/dev/null 2>/dev/null && if [ $? -eq 0 ]; then cat ${rsync_fragments}/header ${rsync_fragments}/frag-* > /etc/rsync.conf; else cat ${rsync_fragments}/header > /etc/rsync.conf; fi; $(exit 0)",
+    command     => "ls ${rsync_fragments}/frag-* 1>/dev/null 2>/dev/null && if [ $? -eq 0 ]; then cat ${rsync_fragments}/header ${rsync_fragments}/frag-* > /etc/rsyncd.conf; else cat ${rsync_fragments}/header > /etc/rsyncd.conf; fi; $(exit 0)",
     subscribe   => File["${rsync_fragments}/header"],
     path        => '/bin:/usr/bin',
   }
